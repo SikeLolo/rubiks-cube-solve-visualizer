fc4cef9e35f09128d415982c2019cbc313d5dd8b
kyan <tbnrthunder9@gmail.com>
2024-08-08 01:57:45 -0400

    C++ backend Server setup port

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "includes/cube.h"
+#include "includes/httplib.h"
+#include "includes/json.hpp"
 using namespace std;
 
 int main(){
     Cube cube;
-    cube.turnFace("RCC", 1);
-    cube.display();
-    cube.turnFace("RC", 1);
-    cube.turnFace("RCC", 2);
-    cube.display();
+    httplib::Server svr;
+    svr.Get("/getCube", [&cube](const httplib::Request& req, httplib::Response& res){
+        //res.set_content("Hello", "text/plain");
+        nlohmann::json j = cube.toJson();
+        res.set_content(j.dump(), "application/json");
+    });
+    svr.listen("0.0.0.0", 8080);
     return 0;
 }
